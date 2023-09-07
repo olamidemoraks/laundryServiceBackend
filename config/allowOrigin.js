e662c681055433edfbbc9685de119cd9b3e3fa7a
olamidemoraks <olamidemoraks@gmail.com>
2023-09-07 02:10:00 +0100

    add to cors urls

--- config/allowOrigin.js
+++ config/allowOrigin.js
@@ -1,3 +1,7 @@
-const allowedOrigin = ["http://localhost:5173", process.env.CORS_URL];
+const allowedOrigin = [
+  "http://localhost:5173",
+  "https://laundryservice-i13l.onrender.com",
+  process.env.CORS_URL,
+];
 
 module.exports = allowedOrigin;
